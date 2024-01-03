e5fa22e31cab95b9c3355b985ae4cd6ebd91c03b
dylan <duanyytop@gmail.com>
2024-01-03 19:14:40 +0800

    add u128ToLe test

--- src/utils/hex.spec.ts
+++ src/utils/hex.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { u128ToLe, u32ToLe, u64ToLe } from './hex'
+import { leToU128, u128ToLe, u32ToLe, u64ToLe } from './hex'
 
 describe('number to little endian', () => {
   it('u32toLe', async () => {
@@ -25,5 +25,10 @@ describe('number to little endian', () => {
   it('u128ToLe', async () => {
     const expected = u128ToLe(BigInt(2100_0000) * BigInt(10 ** 8))
     expect('0040075af07507000000000000000000').toBe(expected)
+  })
+
+  it('u128ToLe', async () => {
+    const expected = leToU128('0x00c05773a57c02000000000000000000')
+    expect(BigInt(700_0000_0000_0000)).toBe(expected)
   })
 })
